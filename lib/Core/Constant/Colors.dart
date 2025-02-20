a0432ad82d5a5fdee8f0f8f43e15ac0b51eebbbf
mohamedeid123i <mohamedeidmohamedeidmohamed@gmail.com>
2025-02-20 11:20:09 +0200

    ..

--- lib/Core/Constant/Colors.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:ui';
-
-abstract class MyColors{
-  static const Color baseColor = Color(0xff555861);
-}
---
+++ lib/Counter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Counter extends StatelessWidget {
+  const Counter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter"),centerTitle: true,),
+      body: Center(
+        child: TweenAnimationBuilder(
+          tween: IntTween(begin: 0,end: 100),
+          duration: Duration(seconds: 5),
+          builder: (context , value , child) {
+            return Text(value.toString(),style: TextStyle(
+                fontSize: 120,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent
+            ),);
+
+          },
+        ),
+      ),
+    );
+  }
+}
